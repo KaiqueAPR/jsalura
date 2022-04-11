052fa9607e005ed664f2cc46dd19f6b9d8da4c12
KaiqueAPR <102837485+KaiqueAPR@users.noreply.github.com>
2022-04-11 13:11:20 -0300

    exemplos aula 1
    exemplos aula 1

---
+++ type-string.js
@@ -0,0 +1,14 @@
+const texto1 = "Olá, Mundo :)";
+const texto2 = 'Olá, mundo';
+const senha = "senhasegura";
+const stringDeNumeros = "32442";
+
+const citacao = "Meu nome é ";
+const meuNome = "Kaique";
+
+//concatenação (+)
+
+console.log(citacao + meuNome)
+
+//template strig ou template literal
+
